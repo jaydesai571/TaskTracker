48c9eb6ca58eaf7f330e93e891a3aced9787a224
Jay Desai <jaydesai571@gmail.com>
2021-03-28 17:57:06 -0400

    Task completed with React-Icons

--- src/App.js
+++ src/App.js
@@ -24,10 +24,16 @@ function App() {
     }
 ])
 
+//Delete task func
+const deleteTask = (id) => {
+  // console.log('delete', id)
+  settasks(tasks.filter((task) => task.id !== id))
+}
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      { tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : ('No Tasks To Show') }
     </div>
   );
 }
